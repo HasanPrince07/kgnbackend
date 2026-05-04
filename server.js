75b7f54e598541b5aacd37754c784b7763c4b067
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-04 17:56:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.static('public', {
 }));
 app.use('/admin', adminRouter);
 app.use('/user', userRouter);
+app.use("/user", (req,res)=>{console.log("backend is colll")})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => { console.log(`server is running on port ${PORT}`) });
