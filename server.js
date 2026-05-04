c9c03c75af0236f7485b64c737a53af8feab6486
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-04 18:08:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static('public', {
 }));
 app.use('/admin', adminRouter);
 //app.use('/user', userRouter);
-app.use("/user/fetchmain",(req,res)=>{res.render("calll abckaendd")})
+app.use("/",(req,res)=>{res.render("calll abckaendd")})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => { console.log(`server is running on port ${PORT}`) });
