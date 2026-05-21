2400d0d066144c0b05e37d7bf27b38bf6a448358
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-21 13:22:52 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     username: { type: String, trim: true },
-    password: { type: String, trim: true, select: false }
+    password: { type: String, trim: true, select: false },
+    email: { type: String, default: "hasanprince0786@gmail.com" },
+    resetPasswordToken: { type: String, default: null },
+    resetPasswordExpires: { type: Date, default: null }
 })
 
 module.exports = mongoose.model('user', userSchema);
